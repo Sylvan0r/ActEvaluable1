c36021cfc154a8b1e657d84965c498a9e2c1a197
sylvanor <adfor409@gmail.com>
2025-10-03 18:11:28 +0100

    Metido dentro de funcion el insertar clase

--- PHP/Games/insertGame.php
+++ PHP/Games/insertGame.php
@@ -3,18 +3,30 @@
     include "../connection.php";
 
     try{
-        if(isset($_FILES["image"]) && $_FILES["image"]["error"] == 0){
-            $image = $_FILES["image"]["tmp_name"];
-            $imgContent = file_get_contents($image);
-            $stmt = $conn->prepare("INSERT INTO games(Título, Descripción, Compañia, Caratula, año, userID) VALUES (?,?,?,?,?,?)");
-            $stmt->bind_param("ssssss", $_POST["name"], $_POST["desc"], $_POST["comp"], $imgContent, $_POST["date"], $_SESSION["user"]);
-            $stmt->execute();
-
-            $_SESSION["exito"] = "Juego creado exitosamente";
-            header("Location: ../../index.php");            
-        }
+        send();
     }catch(PDOException $e){
         $_SESSION["error"] = $e->getMessage();
     }
 
+
+    function nameVal($value){
+        if(isset($value)){
+            return true;
+        }else{      
+            return false;            
+        }
+    }
+
+    function send(){
+        include "../connection.php";
+
+        $image = $_FILES["image"]["tmp_name"];
+        $imgContent = file_get_contents($image);
+        $stmt = $conn->prepare("INSERT INTO games(Título, Descripción, Compañia, Caratula, año, userID) VALUES (?,?,?,?,?,?)");
+        $stmt->bind_param("ssssss", $_POST["name"], $_POST["desc"], $_POST["comp"], $imgContent, $_POST["date"], $_SESSION["user"]);
+        $stmt->execute();
+
+        $_SESSION["exito"] = "Juego creado exitosamente";
+        header("Location: ../../index.php");         
+    }
 ?>
