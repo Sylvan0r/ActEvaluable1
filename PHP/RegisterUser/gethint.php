587557caee9e14c84635233a51225f03cc81edcd
sylvanor <adfor409@gmail.com>
2025-10-10 16:01:10 +0100

    GetHint arreglado

--- PHP/RegisterUser/gethint.php
+++ PHP/RegisterUser/gethint.php
@@ -5,23 +5,33 @@
                    
     if($q != ""){
         if(strlen($q) <= 8){
-            $hint = "<li>Contraseña tiene menos de 8 caracteres</li>";
+            $hint .= "<li style='color:red'>Contraseña tiene menos de 8 caracteres</li>";
+        }else{
+            $hint .= "<li style='color:green'>Contraseña tiene menos de 8 caracteres</li>";
         }
         if(preg_match('/[0-9]/', $q) == 0) { 
-            $hint = "La contraseña debe tener un numero</li>";
-        }       
+            $hint .= "<li style='color:red'>La contraseña debe tener un numero</li>";
+        }else{
+            $hint .= "<li style='color:green'>La contraseña debe tener un numero</li>";
+        }
         if(preg_match('/[a-z]/', $q) == 0) { 
-            $hint = "La contraseña debe tener minusculas</li>";
-        }       
+            $hint .= "<li style='color:red'>La contraseña debe tener minusculas</li>";
+        }else{
+            $hint .= "<li style='color:green'>La contraseña debe tener minusculas</li>";
+        }
         if(preg_match('/[A-Z]/', $q) == 0) { 
-            $hint = "<li>La contraseña debe tener mayusculas</li>";
-        }       
+            $hint .= "<li style='color:red'>La contraseña debe tener mayusculas</li>";
+        }else{
+            $hint .= "<li style='color:green'>La contraseña debe tener mayusculas</li>";
+        }
         if(preg_match('/[^a-zA-Z0-9]/', $q) == 0) { 
-            $hint = "<li>La contraseña debe tener caracteres especiales</li>";
+            $hint .= "<li style='color:red'>La contraseña debe tener caracteres especiales</li>";
+        }else{
+            $hint .= "<li style='color:green'>La contraseña debe tener caracteres especiales</li>";
         }
     } 
 
-    $hint = "</ul>";
+    $hint .= "</ul>";
 
     echo $hint === "" ? "Contraseña cumple los parametros" : $hint;
 ?>
